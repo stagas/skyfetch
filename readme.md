262230f4fe5a8d1817b665178a0a72588501dad4
George Stagas <gstagas@gmail.com>
2020-11-03 00:53:06 +0400

    initial

---
+++ readme.md
@@ -0,0 +1,47 @@
+# skyfetch
+
+rollup + skypack + fetch a module from npm to a single es6-ready file
+
+## example
+
+```sh
+$ npm install -g skyfetch
+...
+$ skyfetch color
+Fetching module from npm: color
+https://cdn.skypack.dev/color
+https://cdn.skypack.dev/-/color@v3.1.3-8J3bFTTZh374x6i2b68U/dist=es2020/color.js
+https://cdn.skypack.dev/-/color-string@v1.5.4-XaLRvunsOYCGjR0MV9KP/dist=es2020/color-string.js
+https://cdn.skypack.dev/new/color-convert@v1.9.3/dist=es2020?from=color
+https://cdn.skypack.dev/-/color-convert@v1.9.3-L1YzLCs5oruHzanPK0Gb/dist=es2020/color-convert.js
+https://cdn.skypack.dev/-/color-name@v1.1.4-7Qy8XzHj4NR72GYiRr2w/dist=es2020/color-name.js
+https://cdn.skypack.dev/new/simple-swizzle@v0.2.2/dist=es2020?from=color-string
+https://cdn.skypack.dev/-/color-name@v1.1.3-0yZtm6UiQGcbxcuUjGRq/dist=es2020/color-name.js
+https://cdn.skypack.dev/-/simple-swizzle@v0.2.2-9tqHQ70Rg3WBBNEjKXxi/dist=es2020/simple-swizzle.js
+https://cdn.skypack.dev/-/is-arrayish@v0.3.2-wwdCi9qjTEI0hc4Amkxb/dist=es2020/is-arrayish.js
+Finished. Output file: color.js
+```
+
+Then simply import it:
+
+`index.html`:
+
+```html
+<script type="module">
+import Color from './color.js'
+document.body.style.background = Color('#7743CE').alpha(.5).darken(.5).string()
+</script>
+```
+
+That's it, no bundlers to install, no complicated configurations. Just an html file and you're ready to start working.
+
+### credits
+
+Thanks to these awesome projects:
+
+[Skypack.dev](https://www.skypack.dev/)
+[rollup](https://rollupjs.org/)
+[rollup-plugin-skypack](https://github.com/yj01jung/rollup-plugin-skypack)
+[rollup-plugin-url-resolve](https://github.com/mjackson/rollup-plugin-url-resolve)
+
+### License MIT
---
+++ skyfetch.js
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+const rollup = require('rollup')
+const Skypack = require('rollup-plugin-skypack/dist/SkypackResolver.js')
+const resolve = require('rollup-plugin-url-resolve')
+wrap(require('http'))
+wrap(require('https'))
+function wrap (http) {
+  const fn = http.request
+  http.request = function (req, ...args) {
+    console.log(req.href)
+    return fn.call(this, req, ...args)
+  }
+}
+const bundleId = '__bundle__'
+const args = process.argv.slice(2).filter(x => x[0] === '-')
+const opts = Object.fromEntries(args.map(x => [x, true]))
+const mod = process.argv.slice(2).filter(x => x[0] !== '-').pop()
+
+if (!mod || mod === '--help' || mod === '-h' || opts['--help'] || opts['-h']) {
+  console.log(
+`\
+usage: skyfetch [opts] <npm-module-name>
+
+options:
+ -o, --output <file>    Output filename (default: <npm-module-name>.js)
+ -f, --format <format>  Rollup output format (amd, cjs, es, iife, umd, system) (default: es)
+ -p, --pinned           Use skypack pinned url (minified) (default: no)\
+`
+  )
+  process.exit(0)
+  return
+}
+
+let optimize = false
+let file = mod + '.js'
+for (const arg of args) {
+  switch (arg) {
+    case '--pinned':
+    case '-p':
+      optimize = true
+      break
+    case '--output':
+    case '--out':
+    case '-o':
+      file = process.argv[process.argv.indexOf(arg)+1]
+      break
+    default:
+      console.error('error: Unrecognized option "' + arg + '".\nTry --help for a list of options.')
+      process.exit(1)
+      return
+  }
+}
+
+const resolver = new Skypack.SkypackResolver({ optimize })
+
+const skypack = {
+  name: 'skypack',
+  async resolveId (id) {
+    if (id === mod) {
+      const url = await resolver.resolve(id)
+      return { id: url }
+    }
+  },
+}
+
+const skyfetch = {
+  name: 'skyfetch',
+  resolveId (source) {
+    if (source === bundleId) {
+      return source
+    }
+    return null
+  },
+  load (id) {
+    if (id === bundleId) {
+      return `
+        export * from '${mod}'
+        export { default } from '${mod}'
+      `
+    }
+    return null
+  }
+}
+
+const inputOptions = {
+  input: '__bundle__',
+  plugins: [
+    skyfetch,
+    skypack,
+    resolve()
+  ]
+}
+
+const outputOptions = {
+  file,
+  format: 'es'
+}
+
+async function build() {
+  console.log('Fetching module from npm:', mod)
+  const bundle = await rollup.rollup(inputOptions)
+  await bundle.write(outputOptions)
+  console.log('Finished. Output file:', file)
+}
+
+build()
